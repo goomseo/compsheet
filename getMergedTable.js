b5e99b64174ce3a91a82ac3c0a2319be41a3f2ba
Minseo Gu <goomseo1743@gmail.com>
2024-10-13 21:40:37 +0900

    Update getMergedTable.js

--- getMergedTable.js
+++ getMergedTable.js
@@ -3,21 +3,33 @@
 function getMergedTable() {
     var targetSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("table_merged");
 
-    var vstackFunc = "=VSTACK(";
+    var vstackFunc1 = "=VSTACK(";
+    var vstackFunc2 = "=VSTACK("
 
-    var row = 2;
+    var row = 5;
     while (targetSheet.getRange(row, 1).isBlank() == false) {
       var compName = targetSheet.getRange(row, 1).getValue();
-      var queryString = "QUERY('" + compName + "'!A3:F, \"SELECT A, B, F\")";
+      
+      var queryString1 = "QUERY('" + compName + "'!A4:H, \"SELECT B, G WHERE (A contains 'TRUE')\")";
+      var queryString2 = "QUERY('" + compName + "'!H4:H, \"SELECT H WHERE H IS NOT NULL\")"
 
-      vstackFunc += queryString;
-      vstackFunc += ", "
+      vstackFunc1 += queryString1;
+      vstackFunc1 += ", "
+
+      vstackFunc2 += queryString2;
+      vstackFunc2 += ", ";
 
       row++;
     }
 
-    vstackFunc = vstackFunc.substring(0, vstackFunc.length - 2);
-    vstackFunc += ")";
+    vstackFunc1 = vstackFunc1.substring(0, vstackFunc1.length - 2);
+    vstackFunc1 += ")";
+
+    vstackFunc2 = vstackFunc2.substring(0, vstackFunc2.length - 2);
+    vstackFunc2 += ")";
     
-    targetSheet.getRange('C2').setFormula(vstackFunc);
+    targetSheet.getRange('C5').setFormula(vstackFunc1);
+    targetSheet.getRange('F5').setFormula(vstackFunc2);
+
 }
+
