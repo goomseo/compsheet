8f78a352a14c523574a0f0f082ea48259fda8863
Minseo Gu <goomseo1743@gmail.com>
2024-11-11 23:41:09 +0900

    Update cleanUp.js

--- cleanUp.js
+++ cleanUp.js
@@ -1,13 +1,13 @@
 function cleanUp() {
   var spreadsheet = SpreadsheetApp.getActive();
 
-  var range = spreadsheet.getRange("BB7:BB");
+  var range = spreadsheet.getRange("BA7:BA");
   range.clearContent();
 
   var lastRow = spreadsheet.getLastRow();
   for (var i = 7; i <= lastRow; i++) {
-    var target = "BB" + i;
-    var formula = getFormulaString("BE" + i, "BC" + i);
+    var target = "BA" + i;
+    var formula = getFormulaString("BD" + i, "BB" + i);
   
     spreadsheet.getRange(target).setFormula(formula);
 }
@@ -15,10 +15,10 @@ function cleanUp() {
   spreadsheet.getRange("I7:I").clearContent();
   spreadsheet.getRange("V7:V").clearContent();
   spreadsheet.getRange("M7:M").clearContent();
-  spreadsheet.getRange("Z7:Z").clearContent();
+  spreadsheet.getRange("Y7:Y").clearContent();
 
   spreadsheet.getRange("K7:L").uncheck();
-  spreadsheet.getRange("X7:Y").uncheck();
+  spreadsheet.getRange("X7:X").uncheck();
 
   var ui = SpreadsheetApp.getUi();
   ui.alert("데이터 청소 완료");
